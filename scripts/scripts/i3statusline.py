d1f67763522298a1f1afcf0fe4aaaf88631edf50
5225225 <5225225@users.noreply.github.com>
2016-03-28 00:13:06 +0100

    Add i3pystatus command (sanitised).

---
+++ scripts/scripts/i3statusline.py
@@ -0,0 +1,66 @@
+import os.path
+
+from i3pystatus import Status
+from i3pystatus.mail import maildir
+from i3pystatus.updates import pacman, cower
+
+def loadcfg(path):
+    return open(os.path.expanduser("~/scripts/data/" + path)).read().strip()
+
+status = Status(standalone=True)
+
+status.register("clock",
+    format=["%H:%M"],
+)
+
+status.register("mpd",
+    format="[<span foreground='#a1b56c'>{artist}</span>] - [<span foreground='#ac4142'>{title}</span>] - [<span foreground='#6a9fb5'>{album}</span>]",
+    max_field_len=0,
+    max_len=0,
+    hints = {"markup": "pango"},
+    interval=1
+)
+
+status.register("mail",
+    backends = [
+        maildir.MaildirMail(
+            directory = os.path.expanduser("~/mail/home/INBOX")
+        ),
+        maildir.MaildirMail(
+            directory = os.path.expanduser("~/mail/school/INBOX")
+        ),
+    ],
+    interval = 1
+)
+
+
+status.register("weather",
+    location_code=loadcfg("location"),
+    colorize="True",
+    format="{current_temp}"
+)
+
+status.register("reddit",
+    username="5225225",
+    password=loadcfg("reddit-password"),
+    mail_brackets=True,
+    interval=60,
+    format="{message_unread}"
+)
+
+status.register("updates",
+    backends = [pacman.Pacman(),
+                cower.Cower(),
+    ],
+    interval=3600,
+)
+
+status.register("github",
+    username="5225225",
+    password=loadcfg("github-password"),
+    interval=300,
+    color="#dddddd"
+)
+
+
+status.run()
