bb70e1e7fa37c41184b398007bad21a1e621cbdf
5225225 <5225225@mailbox.org>
2023-10-14 00:49:17 +0100

    nixfmt

--- mpv.nix
+++ mpv.nix
@@ -1,5 +1,4 @@
-{ pkgs, ... }:
-{
+{ pkgs, ... }: {
   programs.mpv = {
     enable = true;
     scripts = with pkgs.mpvScripts; [
@@ -9,25 +8,25 @@
       sponsorblock
     ];
     config = {
-      cache="auto";
-      demuxer-max-bytes="512MiB";
-      demuxer-readahead-secs=1200;
-      ytdl-format="bestvideo[height<=1080]+bestaudio/best[height<=1080]/best";
-      volume=30;
-      profile="opengl-hq";
-      scale="ewa_lanczossharp";
-      cscale="ewa_lanczossharp";
-      sub-ass-override=false;
-      sub-blur=0.3;
-      sub-color=0.95;
-      sub-border-color="0.05/0.05/0.03";
-      sub-border-size=2.2;
+      cache = "auto";
+      demuxer-max-bytes = "512MiB";
+      demuxer-readahead-secs = 1200;
+      ytdl-format = "bestvideo[height<=1080]+bestaudio/best[height<=1080]/best";
+      volume = 30;
+      profile = "opengl-hq";
+      scale = "ewa_lanczossharp";
+      cscale = "ewa_lanczossharp";
+      sub-ass-override = false;
+      sub-blur = 0.3;
+      sub-color = 0.95;
+      sub-border-color = "0.05/0.05/0.03";
+      sub-border-size = 2.2;
       sub-bold = true;
-      sub-spacing=0.6;
-      sub-margin-x=30;
-      sub-margin-y=36;
-      sub-font-size=43;
-      sub-auto="fuzzy";
+      sub-spacing = 0.6;
+      sub-margin-x = 30;
+      sub-margin-y = 36;
+      sub-font-size = 43;
+      sub-auto = "fuzzy";
     };
   };
 }
