b804bdf0aebb5e93c33c74fca6aed2833bfd6142
5225225 <5225225@mailbox.org>
2023-10-23 17:17:23 +0100

    pin nixpkgs in registry to system nixpkgs, also pin registry

--- flake.nix
+++ flake.nix
@@ -12,6 +12,10 @@
       url = "github:woefe/git-prompt.zsh";
       flake = false;
     };
+    flake-registry = {
+      url = "github:nixos/flake-registry";
+      flake = false;
+    };
   };
 
   outputs =
@@ -21,12 +25,23 @@
     , nix-colors
     , base16-vim
     , git-prompt
+    , flake-registry
     ,
     }: {
       nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-          { system.configurationRevision = self.rev; }
+          { 
+            system.configurationRevision = self.rev;
+
+            # pin nixpkgs to system nixpkgs for determinism
+            nix.registry.nixpkgs.flake = nixpkgs; # For flake commands
+            nix.nixPath = [ "nixpkgs=${nixpkgs}" ]; # For legacy commands
+
+            # thanks to
+            # https://discourse.nixos.org/t/32003/3
+            nix.settings.flake-registry = "${flake-registry}/flake-registry.json";
+          }
           system/configuration.nix
           home-manager.nixosModules.home-manager
           {
