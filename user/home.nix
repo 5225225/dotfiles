e812afb62804b4aaafc3ed2f6d61b90d57a55b76
5225225 <5225225@mailbox.org>
2024-04-02 14:12:14 +0100

    add nicotine-plus

--- user/home.nix
+++ user/home.nix
@@ -78,6 +78,7 @@
     mpc-cli
     mpdscribble
     ncdu
+    nicotine-plus
     nixfmt
     pavucontrol
     prismlauncher
@@ -87,7 +88,6 @@
     rxvt-unicode
     sgt-puzzles
     shattered-pixel-dungeon
-    soulseekqt
     starsector
     stow
     tamzen
@@ -102,22 +102,6 @@
     xdg-utils
     yt-dlp
     zola
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
