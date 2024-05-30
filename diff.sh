0d0c6292d49e56121c25b2860e86094f662a078b
5225225 <5225225@mailbox.org>
2024-05-30 12:17:20 +0100

    add funny little diff script

---
+++ diff.sh
@@ -0,0 +1,7 @@
+OLD=$(git rev-parse $1)
+NEW=$(git rev-parse $2)
+
+nix build "git+file:///home/jess/dotfiles?rev=$OLD#nixosConfigurations.nixos.config.system.build.toplevel" --out-link old --quiet
+nix build "git+file:///home/jess/dotfiles?rev=$NEW#nixosConfigurations.nixos.config.system.build.toplevel" --out-link new --quiet
+
+nix store diff-closures ./old ./new
