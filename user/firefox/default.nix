83fa671140312622a6646179cf11610b9edfd8d9
5225225 <5225225@mailbox.org>
2025-02-12 06:06:16 +0000

    set up some firefox policies

--- user/firefox/default.nix
+++ user/firefox/default.nix
@@ -4,5 +4,36 @@
     profiles."1a0nke3z.default" = {
       userChrome = builtins.readFile ./userChrome.css;
     };
+    policies = {
+      AutofillAddressEnabled = false;
+      AutofillCreditCardEnabled = false;
+      DisableFirefoxStudies = true;
+      DisablePocket = true;
+      DisableSetDesktopBackground = true;
+      DisableTelemetry = true;
+      DNSOverHTTPS = {
+        # Use system DNS instead, it *is* encrypted.
+        Enabled = false;
+        Locked = true;
+      };
+
+      DontCheckDefaultBrowser = true;
+      EnableTrackingProtection = {
+        Value = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+        EmailTracking = true;
+        Locked = true;
+      };
+
+      UserMessaging = {
+        ExtensionRecommendations = false;
+        FeatureRecommendations = false;
+        UrlbarInterventions = false;
+        SkipOnboarding = true;
+        MoreFromMozilla = false;
+        Locked = true;
+      };
+    };
   };
 }
