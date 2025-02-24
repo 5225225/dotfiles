39f42426ef7ef493b9e96dfa0455f85210dbb9d2
5225225 <5225225@mailbox.org>
2025-02-24 15:27:24 +0000

    use nixos-rebuild-ng

--- system/configuration.nix
+++ system/configuration.nix
@@ -162,6 +162,7 @@
       pkgs.vimHugeX
       pkgs.man-pages
       pkgs.man-pages-posix
+      pkgs.git
     ];
   };
 
@@ -179,7 +180,10 @@
     polkit.enable = true;
 
     # https://github.com/NixOS/nixpkgs/issues/361592#issuecomment-2516342739
-    pam.services.systemd-run0 = {};
+    pam.services.systemd-run0 = {
+      setEnvironment = true;
+      pamMount = false;
+    };
   };
 
   services.pipewire = {
@@ -206,15 +210,11 @@
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
-
-  system.nixos.label = "cfg:${builtins.substring 0 10 config.system.configurationRevision}${config.system.nixos.versionSuffix}";
+  system = {
+    stateVersion = "23.05"; # Did you read the comment?
+    nixos.label = "cfg:${builtins.substring 0 10 config.system.configurationRevision}${config.system.nixos.versionSuffix}";
+    rebuild.enableNg = true;
+  };
 
   xdg.portal = {
     enable = true;
