d53b1084eea2c83e1c39df5ef54e734e33292fab
5225225 <5225225@mailbox.org>
2025-03-02 14:50:33 +0000

    remove dupe setting, keep fish git prompt settings sorted

--- user/fish/default.nix
+++ user/fish/default.nix
@@ -35,22 +35,21 @@
           body =
             #fish
             ''
-              set -g __fish_git_prompt_show_informative_status 1
-              set -g __fish_git_prompt_showuntrackedfiles 1
-              set -g __fish_git_prompt_showdirtystate 1
-              set -g __fish_git_prompt_showuntrackedfiles 1
-              set -g __fish_git_prompt_showstashstate 1
-              set -g __fish_git_prompt_showcolorhints 1
-
+              # keep-sorted start
               set -g __fish_git_prompt_char_dirtystate "+"
+              set -g __fish_git_prompt_char_stagedstate '+'
+              set -g __fish_git_prompt_char_stashstate '$'
+              set -g __fish_git_prompt_char_untrackedfiles '.'
               set -g __fish_git_prompt_char_upstream_ahead "^"
               set -g __fish_git_prompt_char_upstream_behind "v"
-              set -g __fish_git_prompt_char_stashstate '$'
-              set -g __fish_git_prompt_char_stagedstate '+'
-              set -g __fish_git_prompt_char_untrackedfiles '.'
-
               set -g __fish_git_prompt_color_branch --bold magenta
               set -g __fish_git_prompt_color_cleanstate --bold green
+              set -g __fish_git_prompt_show_informative_status 1
+              set -g __fish_git_prompt_showcolorhints 1
+              set -g __fish_git_prompt_showdirtystate 1
+              set -g __fish_git_prompt_showstashstate 1
+              set -g __fish_git_prompt_showuntrackedfiles 1
+              # keep-sorted end
 
               fish_vcs_prompt " [%s]"
             '';
