d327e3ba61b8127b929f3eba8c239cd1dd817f15
5225225 <5225225@mailbox.org>
2025-03-03 23:35:38 +0000

    vim: add mkdnflow/table mode plugins

--- user/neovim.nix
+++ user/neovim.nix
@@ -47,13 +47,55 @@ in
           "DiffText"
         ];
       };
+      mkdnflow = {
+        enable = true;
+        extraOptions.new_file_template = {
+          use_template = true;
+          template = ''
+            +++
+            title = "{{title}}"
+            +++
+          '';
+        };
+        links = {
+          implicitExtension = "md";
+          # TODO: this assumes i only use this for my site
+          # might want to adjust this to be per-directory
+          # this works fine for now though.
+          transformExplicit =
+            #lua
+            ''
+              function(text)
+                text = text:gsub(" ", "-")
+                text = text:lower()
+                text = "@/w/"..text..".md"
+                return(text)
+              end
+            '';
+          transformImplicit =
+            #lua
+            ''
+              function(text)
+                text = string.gsub(text, "@/w/", "", 1)
+                return(text)
+              end
+            '';
+        };
+
+        modules = {
+          tables = false;
+        };
+      };
     };
     colorschemes.base16 = {
       enable = true;
       colorscheme = "tube";
       setUpBar = true;
     };
-    extraPlugins = [ vp.nvim-lspconfig ];
+    extraPlugins = [
+      vp.nvim-lspconfig
+      vp.vim-table-mode
+    ];
     opts = {
       mouse = "";
       expandtab = true;
