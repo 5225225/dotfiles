413fdf88795ccf5a2326f34847f9b790a1aad3bd
5225225 <5225225@mailbox.org>
2025-03-12 20:06:58 +0000

    keep flake inputs

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    blank.url = "github:divnix/blank";
     # nixos-unstable-small.url = "github:NixOS/nixpkgs/nixos-unstable-small";
     home-manager = {
       url = "github:nix-community/home-manager/master";
@@ -23,7 +24,13 @@
       url = "github:ryantm/agenix";
       inputs = {
         nixpkgs.follows = "nixpkgs";
-        darwin.follows = "";
+
+        # NOTE: With the "keep flake inputs" thing
+        # (https://github.com/NixOS/nix/issues/3995#issuecomment-2081164515)
+        # you *cannot* use just "" to blank out a dependency.
+        # so use blank (defined above) here.
+        # https://matrix.to/#/!RRerllqmbATpmbJgCn:nixos.org/$GL3qXaUWqtKjPo09zZ-dElwUIz_oIXc02nZdVgNzrio?via=lossy.network&via=matrix.org&via=tchncs.de
+        darwin.follows = "blank";
         home-manager.follows = "home-manager";
       };
     };
@@ -46,14 +53,20 @@
     let
       inputs = inputs' // {
         firefox-addons = inputs'.firefox-addons.packages.x86_64-linux;
-        nixos-unstable-small = (import inputs'.nixos-unstable-small) { system = "x86_64-linux"; };
+        # nixos-unstable-small = (import inputs'.nixos-unstable-small) { system = "x86_64-linux"; };
       };
       treefmtEval = inputs.treefmt-nix.lib.evalModule nixpkgs.legacyPackages.x86_64-linux ./treefmt.nix;
+      collectFlakeInputs =
+        input:
+        [ input ] ++ builtins.concatMap collectFlakeInputs (builtins.attrValues (input.inputs or { }));
     in
     {
       nixosConfigurations.iridium = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        modules = [ system/configuration.nix ];
+        modules = [
+          system/configuration.nix
+          { system.extraDependencies = builtins.concatMap collectFlakeInputs (builtins.attrValues inputs'); }
+        ];
         specialArgs = {
           inherit inputs;
           inherit self;
