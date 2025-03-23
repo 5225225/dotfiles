08b22a1a0892fd297b2c2b12827e37f4de1f78c6
5225225 <5225225@mailbox.org>
2025-03-23 11:42:32 +0000

    mpd: some user service hardening

--- user/mpd.nix
+++ user/mpd.nix
@@ -16,4 +16,39 @@
       auto_update "yes"
     '';
   };
+
+  systemd.user.services.mpd.Service = {
+    KeyringMode = "private";
+    LockPersonality = true;
+    MemoryDenyWriteExecute = true;
+    NoNewPrivileges = true;
+    PrivateMounts = "yes";
+    PrivateTmp = "yes";
+    ProtectControlGroups = true;
+    ProtectHostname = true;
+    ProtectKernelModules = true;
+    ProtectKernelTunables = true;
+    ProtectSystem = "strict";
+    ReadWritePaths = [ config.services.mpd.dataDir ];
+    ReadOnlyPaths = [ config.services.mpd.musicDirectory ];
+    RemoveIPC = true;
+    RestrictAddressFamilies = [
+      "AF_UNIX"
+      "AF_INET"
+      "AF_INET6"
+    ];
+    RestrictRealtime = true;
+    RestrictSUIDSGID = true;
+    PrivateUsers = true;
+    ProtectProc = "noaccess";
+    CapabilityBoundingSet = "";
+    PrivateDevices = true;
+    ProtectClock = true;
+    ProtectKernelLogs = true;
+
+    # TODO: @default isn't enough here and i don't care enough to find out why
+    # bash fails on it, in any case
+    SystemCallFilter = "@system-service";
+    SystemCallArchitectures = "native";
+  };
 }
